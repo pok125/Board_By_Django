179a45235537f6870e15be68d6e365954a91f7cd
kwangho <pokljh0411@gmail.com>
2024-09-03 14:31:57 +0900

    post 모델 작업

---
+++ backend_board/Board/posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    files = models.FileField()
+    views = models.IntegerField(default=0, null=False, verbose_name='조회수')
+    created_at = models.DateField(auto_now_add=True, verbose_name='등록일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
