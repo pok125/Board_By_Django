d93345899716f00d2748fdfcee108a06068a6b8d
kwangho <pokljh0411@gmail.com>
2024-09-11 15:43:23 +0900

    게시글 render 분리 작업 및 url데이터 분리

--- frontend_board/js/home.js
+++ frontend_board/js/home.js
@@ -1,4 +1,5 @@
 import { apiGet } from "./fetchAPI.js";
+import * as URL from "./urls.js";
 
 const $chatBtn = document.getElementById("btn-chat");
 const $loginBtn = document.getElementById("btn-login");
@@ -9,26 +10,69 @@ const $search = document.getElementById("search");
 const $searchBtn = document.getElementById("btn-search");
 const $category = document.getElementById("category");
 const $sort = document.getElementById("sort");
+const $emptyText = document.getElementById("empty-text");
+const $postList = document.getElementById("post-list");
 
 let postCount = 0;
 let categoryList = ["전체", "공지사항", "일반"];
 let sortList = ["조회순", "최신순"];
 
-$mainDesc.innerHTML = `<strong>${postCount}개</strong>의 게시글이 있습니다.`;
-
-for (let i = 0; i < categoryList.length; i++) {
-    const option = document.createElement("option");
-    option.value = categoryList[i];
-    option.textContent = categoryList[i];
-    $category.appendChild(option);
+async function loadPost() {
+    try {
+        const datas = await apiGet(URL.homeURL);
+    
+        return datas;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-for (let i = 0; i < sortList.length; i++) {
-    const option = document.createElement("option");
-    option.value = sortList[i];
-    option.textContent = sortList[i];
-    $sort.appendChild(option);
+function render() {
+    const datas = loadPost();
+    postCount = datas.length();
+
+    $mainDesc.innerHTML = `<strong>${postCount}개</strong>의 게시글이 있습니다.`;
+
+    for (let i = 0; i < categoryList.length; i++) {
+        const option = document.createElement("option");
+        option.value = categoryList[i];
+        option.textContent = categoryList[i];
+        $category.appendChild(option);
+    }
+    
+    for (let i = 0; i < sortList.length; i++) {
+        const option = document.createElement("option");
+        option.value = sortList[i];
+        option.textContent = sortList[i];
+        $sort.appendChild(option);
+    }
+
+    if (postCount === 0) {
+        
+
+    }
+    else {
+        $emptyText.style.display = "none";
+        createPost(datas);
+    }
+
 }
 
-const homeURL = "http://127.0.0.1:8000/";
-const res = await apiGet(homeURL);
+function createPost(datas) {
+    
+    for (let i = 0; i < postCount; i++) {
+        const $post = document.createElement("tr");
+
+        const $checkBox = document.createElement("td");
+        const $postNum = document.createElement("td");
+        const $title = document.createElement("td");
+        const $writer = document.createElement("td");
+        const $date = document.createElement("td");
+        const $viewCount = document.createElement("td");
+
+        $post.append($checkBox, $postNum, $title, $writer, $date, $viewCount);
+        $postList.appendChild($post);
+    }
+}
+
+render();
---
+++ frontend_board/js/urls.js
@@ -0,0 +1,1 @@
+export const homeURL = "http://127.0.0.1:8000";
